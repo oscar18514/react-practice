b3e45eb0723766d4011cd9553dffa736d1e31f24
oscar18514 <sanchezgioscarivan67@gmail.com>
2025-09-01 21:50:57 -0500

    subir archivo inicial

---
+++ index.js
@@ -0,0 +1,15 @@
+const words = ["oso",
+  "ala",
+  "anilina",
+  "reconocer",
+  "radar",
+  "salas",
+  "somos",
+  "oro",
+  "neuquen"]
+
+ 
+  function checkPalindrome(words){
+words.forEach(word => console.log(word))
+}
+checkPalindrome(words)
